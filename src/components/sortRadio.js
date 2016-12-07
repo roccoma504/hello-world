f51d55c7a17306390855be4539d13872f0b78d50
Matt Rocco <roccoma504@gmail.com>
2016-12-06 20:52:56 -0500

    added region filter

--- src/components/sortRadio.js
+++ src/components/sortRadio.js
@@ -1,35 +1,57 @@
 import React from 'react'; 
+import Checkbox from 'material-ui/Checkbox';
 import {RadioButton, RadioButtonGroup} from 'material-ui/RadioButton';
 
 const styles = {
-  block: {
+radioButtonGroup: {
+    marginBottom: 10,
+    marginLeft: 5,
+    width: "auto",
+    padding: 10,
+    labelPosition:'left',
+    display: "flex",
+    maxWidth: 500
+  },
+  radioButtonSmall: {
+    marginBottom: 10,
+    marginLeft: 5,
+    width: 250,
+    padding: 10,
+    labelPosition:'left',
+    display: "flex",
+    maxWidth: 4000
+  },
+radioButtonLarge: {
+    marginBottom: 10,
+    marginLeft: 5,
+    width: 400,
+    padding: 10,
+    labelPosition:'left',
+    display: "flex",
+    maxWidth: 4000
+  },
+  checkbox: {
+    marginBottom: 16,
     maxWidth: 500,
   },
-  radioButton: {
-    marginBottom: 16,
-    marginLeft: 0,
-    width: "auto",
-    padding: 10
-  },
+  content:{
+    flex:1,
+    flexDirection:'row',
+    alignItems:'center',
+    justifyContent:'center'
+    }
 };
 
 // MUI radio buttons. Used for sorting.
 // TODO: Pass in more prop info if used again.
 export const SortRadioButton = (props) => (
-  <div>
-    <RadioButtonGroup name='sort' defaultSelected='name' onChange={props.change} style={{display: "flex"}}>
-      <RadioButton
-        value='name'
-        label='Name'
-        style={styles.radioButton}
-      />
-      <RadioButton
-        value='density'
-        label='Density'
-        style={styles.radioButton}
-      />
-    </RadioButtonGroup> 
-  </div>
+    <div>
+        <Checkbox label="Group Regions" style={styles.checkbox} onCheck={props.onCheck}/>
+        <RadioButtonGroup name={props.name} defaultSelected={props.defaultSelected} onChange={props.onChange} style={styles.radioButtonGroup}>
+            <RadioButton value={props.value1} label={props.label1}style={styles.radioButtonSmall}/>
+            <RadioButton value={props.value2} label={props.label2}style={styles.radioButtonLarge}/>
+        </RadioButtonGroup> 
+    </div>
 );
 
 export default SortRadioButton;
